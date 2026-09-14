5e3a703f9e38e79906e85e386942f8186e641a15
YasminLuna <65403619+YasminLuna@users.noreply.github.com>
2026-09-14 11:07:31 -0300

    fix: corrige sintaxe das variáveis Terraform

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,31 @@
-variable "aws_region" { type = string, default = "us-east-1" }
-variable "environment" { type = string, default = "hml" }
-variable "vpc_id" { type = string }
-variable "private_subnet_ids" { type = list(string) }
-variable "vpc_cidr" { type = string }
-variable "db_name" { type = string, default = "oficina_db" }
-variable "db_user" { type = string, default = "oficina" }
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "environment" {
+  type    = string
+  default = "hml"
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "private_subnet_ids" {
+  type = list(string)
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "db_name" {
+  type    = string
+  default = "oficina_db"
+}
+
+variable "db_user" {
+  type    = string
+  default = "oficina"
+}
